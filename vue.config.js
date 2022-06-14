4b5c3f4a92c2a151c3c5d62ad58428b5f3925ce2
garyabc0123 <garyabc0123@gmail.com>
2022-06-15 07:26:12 +0800

    clear

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ],
-  publicPath: process.env.NONE_ENV === 'production' ? '/CS151-2/' :'/'
-})
