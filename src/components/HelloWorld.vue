a3273be16ee54c2307ce46b2ad0e75af46e1a229
garyabc0123 <garyabc0123@gmail.com>
2022-06-15 06:38:16 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,124 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar
+        app
+        color="white"
+        flat
+    >
+      <!--      圖標-->
+      <v-container class="py-0 fill-height">
+        <v-avatar
+            class="mr-10"
+            color="grey darken-1"
+            size="32"
+        >
+          <img
+
+              alt="Avatar"
+              src="https://e.ecimg.tw/img/icon/24-32x32.ico"
+          >
+        </v-avatar>
+
+        <!--        留白-->
+        <v-spacer></v-spacer>
+        <!--        搜索框-->
+        <v-responsive max-width="260">
+          <v-text-field
+              dense
+              flat
+              hide-details
+              rounded
+              solo-inverted
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <!--          left-->
+          <v-col cols="3">
+            <v-sheet rounded="lg">
+              <v-list rounded>
+                <v-list-item-group
+                    v-model="model"
+                    multiple
+                >
+                  <template v-for="(item, i) in diskType">
+                    <v-divider
+                        v-if="!item"
+                        :key="`divider-${i}`"
+                    ></v-divider>
+
+                    <v-list-item
+                        v-else
+                        :key="`item-${i}`"
+                        :value="item"
+                        active-class="deep-purple--text text--accent-4"
+                    >
+                      <template v-slot:default="{ active }">
+                        <v-list-item-content>
+                          <v-list-item-title v-text="item"></v-list-item-title>
+                        </v-list-item-content>
+
+                        <v-list-item-action>
+                          <v-checkbox
+                              :input-value="active"
+                              color="deep-purple accent-4"
+                          ></v-checkbox>
+                        </v-list-item-action>
+                      </template>
+                    </v-list-item>
+                  </template>
+                </v-list-item-group>
+
+
+              </v-list>
+
+
+            </v-sheet>
+          </v-col>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          <!--          right-->
+
+          <v-col>
+            <v-sheet
+                min-height="70vh"
+                rounded="lg"
+            >
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data: () => ({
+    diskType: [
+      '3.5吋機械硬碟',
+      '2.5吋機械硬碟',
+      '2.5吋固態硬碟',
+      'Gen3 固態硬碟',
+      'Gen4 固態硬碟'
+    ],
+  }),
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
